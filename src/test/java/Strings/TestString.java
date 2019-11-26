61582c45f4ee65a651e627e634a4f8784e0f5abe
zirfee <zirfee01@gmail.com>
2019-11-27 00:19:15 +0800

    整数操作，溢出操作

--- src/test/java/Strings/TestString.java
+++ src/test/java/Strings/TestString.java
@@ -8,4 +8,10 @@ public class TestString {
         z字变换 z = new z字变换();
         System.out.println(z.convert("PAYPALISHIRING",3));
     }
+   @Test
+   public void t1(){
+       字符串里取整数 z= new 字符串里取整数();
+        System.out.println(z.myAtoi(" -42"));
+
+   }
 }
